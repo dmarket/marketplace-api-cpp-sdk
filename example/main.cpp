10e186406fd9ecd312875b36c186fe458ff077ba
Alexander Matsenko <alexx.2409@gmail.com>
2020-01-28 16:23:42 +0200

    intial

---
+++ example/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <grpcpp/grpcpp.h>
+
+#include "api.pb.h"
+#include "api.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using marketplace::SignInRequest;
+using marketplace::AuthResponse;
+using marketplace::MarketAPI;
+
+class ApiClient {
+public:
+    ApiClient(std::shared_ptr<Channel> channel) : stub_(MarketAPI::NewStub(channel)) {}
+
+    // Assembles the client's payload, sends it and presents the response back
+    // from the server.
+    std::string Login(const std::string &email, const std::string &password) {
+        SignInRequest request;
+        request.set_email(email);
+        request.set_password(password);
+
+        // Container for the data we expect from the server.
+        AuthResponse reply;
+
+        // Context for the client. It could be used to convey extra information to
+        // the server and/or tweak certain RPC behaviors.
+        ClientContext context;
+
+        Status status = stub_->SignIn(&context, request, &reply);
+
+        // Act upon its status.
+        if (status.ok()) {
+            return reply.authtoken();
+        } else {
+            return "RPC error";
+        }
+    }
+
+private:
+    std::unique_ptr<MarketAPI::Stub> stub_;
+};
+
+
+int main() {
+    ApiClient api(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
+
+    std::string email("load-testing_2611-0@gmail.com");
+    std::string password("QWERTYasdf1234zxcv");
+
+    std::string auth_token = api.Login(email, password);
+    std::cout << "Token received: " << auth_token << std::endl;
+
+    return 0;
+}
